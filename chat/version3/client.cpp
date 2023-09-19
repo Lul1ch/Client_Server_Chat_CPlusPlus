6d5ba3558c9b36de0801e1fd2eec8805f08bcd81
Ilya <ilya@localhost.localdomain>
2023-09-19 12:54:25 +0300

    Update client.cpp, still some critical bug

--- chat/version3/client.cpp
+++ chat/version3/client.cpp
@@ -1,7 +1,7 @@
 #include <sys/types.h>
 #include <iostream>
 #include <netinet/in.h>
-//#include <bits/stdc++.h>
+#include <bits/stdc++.h>
 #include <string>
 
 #include <string.h>
@@ -13,19 +13,23 @@
 #include <map>
 #include <thread>
 #include <chrono>
+#include <mutex>
 using namespace std;
 
 #define PORT 1504
 #define BUFSIZE 1024
 
 bool isExit = false;
-string inputStr;
-bool isMessage;
-bool isCommand;
-int networkSocket = -1;
 
 class ConnectionsManager
 {
+   bool isMessage;
+   mutex isMessageMutex;
+   int networkSocket;
+   string inputStr;
+   
+   thread reciver;
+   thread client;  
    
    void MessagesListener() 
    {
@@ -34,7 +38,7 @@ class ConnectionsManager
          if(networkSocket != -1) {
             int lastIndex = recv(networkSocket, (char*)buffer,BUFSIZE,0);
             buffer[lastIndex] = '\0';
-            cout <<  buffer << "\n";
+            cout << "\n" <<  buffer << "\n";
          }
 
       }
@@ -63,27 +67,44 @@ class ConnectionsManager
          cout << "An error occurred during the attempt to establish the connection\n";
          isExit = true;
       }    
-
+      isMessage = false;
       while (!isExit)
       {
-         if(isMessage && !isExit)
+         if(isMessage)
          {
+            lock_guard<mutex> guard(isMessageMutex);
+            cout << "Hello from messages thread " << networkSocket << "\n";
             send(networkSocket, inputStr.c_str(), inputStr.length(),0);
             isMessage = false;
+            //isMessageMutex.unlock();
          } else {
-            this_thread::sleep_for(chrono::milliseconds(2000));
+            this_thread::sleep_for(chrono::milliseconds(1000));
          }
       }
      
-      close(networkSocket);
-      pthread_exit(NULL);  
+      close(networkSocket);  
    }
 public:
    ConnectionsManager()
    {
+
+   }
+   ConnectionsManager(int i)
+   {
+      this->isMessage = false;
+      this->networkSocket = -1;
+
       ConnectionsManager tempObj;
-      thread reciver(&ConnectionsManager::MessagesListener, &tempObj);
-      thread client(&ConnectionsManager::ClientThread, &tempObj);
+      reciver = thread(&ConnectionsManager::MessagesListener, &tempObj);
+      client = thread(&ConnectionsManager::ClientThread, &tempObj);
+   }
+
+   void IsMessageToSend(string inputStr)
+   {
+      lock_guard<mutex> guard(isMessageMutex);
+      isMessage = true;
+      this->inputStr = inputStr;
+      cout << "Message executed\n";
    }
 };
 
@@ -104,31 +125,46 @@ public:
    }
 }*/
 class CommandsHandler
-{  
-  /* map<string, Command*> funcMap = {
+{ 
+   ConnectionsManager* manager;
+   bool isCommand;
+   mutex isCommandMutex;
+   string inputStr;
+ 
+   thread handler;  
+   /* map<string, Command*> funcMap = {
       {"/exit", new Exit}
    };*/
-   void Exit() {
+   void Exit() 
+   {
+      manager->IsMessageToSend(inputStr);
       isExit = true;
    }
-   void ExecuteCommand(string commandName) {
-      if (commandName == "/exit") {
-         Exit();     
+   void ExecuteCommand(string commandName) 
+   {
+      if(commandName == "/exit")
+      {
+         Exit();
+      } else {
+         cout << "\nUnknown command\n"; 
       }
    }
    void CommandsHandlerRoutine()
    {
-      
+      isCommand = false;
       while(!isExit)
-      {
-         if(isCommand && !isExit)
+      {      
+         if(isCommand)
          {
+            cout << "Hello from command thread\n";
+            lock_guard<mutex> guard(isCommandMutex);
             string commandName = inputStr.substr(0, inputStr.find(" "));
-           //funcMap[commandName]->Execute();           
+           //funcMap[commandName]->Execute();
             ExecuteCommand(commandName);
             isCommand = false;
+            //isCommandMutex.unlock();
          } else {
-            this_thread::sleep_for(chrono::milliseconds(2000));
+            this_thread::sleep_for(chrono::milliseconds(1000));
          }
       }
    }
@@ -136,13 +172,30 @@ class CommandsHandler
 public:
    CommandsHandler()
    {
+
+   }
+   CommandsHandler(ConnectionsManager* manager)
+   {
+      this->manager = manager;
+      this->isCommand = false;
       CommandsHandler tempObj;
-      thread handler(&CommandsHandler::CommandsHandlerRoutine, &tempObj);
+      handler = thread(&CommandsHandler::CommandsHandlerRoutine, &tempObj);
+   }
+   void IsCommandToHandle(string inputStr)
+   {
+      lock_guard<mutex> guard(isCommandMutex);
+      isCommand = true;
+      this->inputStr = inputStr;
+      cout << "Command executed\n";
    }   
 };
 
 class InputHandler
 {
+   ConnectionsManager* manager;
+   CommandsHandler* handler;
+   string inputStr;
+   
    bool IsItCommand(string str)
    {
       return str[0] == '/';
@@ -155,16 +208,21 @@ class InputHandler
          getline(cin, inputStr);
          if (IsItCommand(inputStr))
          {
-            isCommand = true;
+            cout << "Command\n";
+            handler->IsCommandToHandle(inputStr);
          } else 
-         { 
-            isMessage = true;
+         {
+            cout << "Message\n";
+            manager->IsMessageToSend(inputStr);
          }
+         
       }
    }
 public:
-   InputHandler()
+   InputHandler(ConnectionsManager* manager, CommandsHandler* handler)
    {
+      this->manager = manager;
+      this->handler = handler;
       InputListener();            
    }
    
@@ -172,12 +230,12 @@ public:
 
 int main (int argc, char* argv[])
 {
-   ConnectionsManager* conManager = new ConnectionsManager();
-   CommandsHandler* commsHandler = new CommandsHandler(); 
-   InputHandler* input = new InputHandler();
-   
+   ConnectionsManager* conManager = new ConnectionsManager(1);
+   CommandsHandler* commandsHandler = new CommandsHandler(conManager);
+   InputHandler* input = new InputHandler(conManager, commandsHandler);
+
    delete conManager;
-   delete commsHandler;
+   delete commandsHandler;
    delete input;
    return 0;
 }
